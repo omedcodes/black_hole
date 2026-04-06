e26202944266420b8e2cd18d7a1b2b84cb1a1069
omedcodes <omedsafi21@gmail.com>
2026-04-06 20:36:06 +0200

    Feat: add StartGLFW window function and shader program function

--- raytracing_2d.cpp
+++ raytracing_2d.cpp
@@ -29,5 +29,97 @@ public:
         this->quadVAO = result[0];
         this->texture = result[1];
     }
-    GLFWwindow* StartGLFW();
+    GLFWwindow* StartGLFW(){
+        if(!glfwInit()){
+            std::cerr << "glfw failed init, oh nooo PANIC!"<<std::endl;
+            return nullptr;
+        }
+
+        GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "ray tracer", NULL, NULL);
+        glfwMakeContextCurrent(window);
+
+        glewExperimental = GL_TRUE;
+        if (glewInit() != GLEW_OK){
+            std::cerr << "Failed to initialize GLEW." << std::endl;
+            glfwTerminate();
+            return nullptr;
+        }
+
+        glViewport(0, 0, WIDTH, HEIGHT);
+        return window;
+    };
+    GLuint CreateShaderProgram(){
+        const char* vertexShaderSource = R"(
+        #version 330 core
+        layout (location = 0) in vec2 aPos;
+        layout (location = 1) in vec2 aTexCoord;
+        out vec2 TexCoord;
+        void main(){
+            gl_Position = vec4(aPos, 0.0, 1.0); // z=0
+            TexCoord = aTexCoord;
+        })";
+
+        const char* fragmentShaderSource = R"(
+        #version 330 core
+        in vec2 TexCoord;
+        out vec4 FragColor;
+        uniform sampler2D screenTexture;
+        void main(){
+            FragColor = texture(screenTexture, TexCoord);
+        })";
+
+        //~ vertex shader
+        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+        //~ fragment shader
+        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+
+        GLuint shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        glDeleteProgram(vertexShader);
+        glDeleteProgram(fragmentShader);
+
+        return shaderProgram;
+    };
+
+    std::vector<GLuint> QuadVAO(){
+        float quadVertices[] = {
+            // positions   // texCoords
+            -1.0f,  1.0f,  0.0f, 1.0f,  // top left
+            -1.0f, -1.0f,  0.0f, 0.0f,  // bottom left
+            1.0f, -1.0f,  1.0f, 0.0f,  // bottom right
+
+            -1.0f,  1.0f,  0.0f, 1.0f,  // top left
+            1.0f, -1.0f,  1.0f, 0.0f,  // bottom right
+            1.0f,  1.0f,  1.0f, 1.0f   // top right
+        };
+
+        GLuint VAO, VBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        std::vector<GLuint> VAOtexture = {VAO, texture};
+        return VAOtexture;
+    };
+    void renderScene(std::vector<unsigned char> pixels);
 };
